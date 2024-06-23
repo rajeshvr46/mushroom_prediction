3579957603a2ce940571c90889629adbfcedc6d4
Rajesh <rajeshrajeshvr46@gmail.com>
2024-06-23 14:39:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import joblib
 from label import encode
 st.set_page_config(page_title='Mushroom Prediction')
 st.title('Mushrooms prediction🍄')  
